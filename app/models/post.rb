fb4d3495521310728538222a798f66cc2be2541c
Nathakorn Chaninthanadecha <nathakorn.tonch@gmail.com>
2021-12-27 13:59:48 +0700

    Fix test and enable gem 'redis'

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true
   has_rich_text :content
--- test/mailers/comments_mailer_test.rb
+++ test/mailers/comments_mailer_test.rb
@@ -2,11 +2,10 @@ require "test_helper"
 
 class CommentsMailerTest < ActionMailer::TestCase
   test "submitted" do
-    mail = CommentsMailer.submitted
-    assert_equal "Submitted", mail.subject
-    assert_equal ["to@example.org"], mail.to
+    mail = CommentsMailer.submitted comments(:one)
+    assert_equal "New comment!", mail.subject
+    assert_equal ["blog-owner@example.org"], mail.to
     assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
   end
 
 end
